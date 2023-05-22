d27d2a17cbccea8a52e77a5fc02fc8f9911b9e12
Nathan <ospinonathan@gmail.com>
2023-05-22 16:23:47 -0500

    "Avances"

--- Logica.js
+++ Logica.js
@@ -1,32 +1,33 @@
 let K;
+let comboTiempoGeneral = document.getElementById("tiempoGeneral").value;
+var image1 = document.getElementById("img1");
+var image2 = document.getElementById("img2");
 
 function calculoK() {
     let tHalf = document.getElementById('t1/2').value;
     let T = document.getElementById('T').value;
-    let Nt = document.getElementById('Nt').value;
 
     if (tHalf !== '') {
-        K = Math.log(2) / tHalf;
-        mostrarResultadoK("K: " + K);
+        K = -Math.log(2) / tHalf;
+        mostrarResultadoK(K);
+        image1.style.display = "none";
+        image2.style.display = "block";
     } else if (T !== '') {
-        K = 1 / T;
-        mostrarResultadoK("K: " + K);
-    } else if (Nt !== '') {
-        let n0 = document.getElementById('n0').value;
-        let t = document.getElementById('t').value;
-        let ln = Nt / n0;
-        let result = Math.log(ln) / -t;
-        K = result;
-        mostrarResultadoK("K: " + K.toFixed(3));
-    } else {
-        // Alguno de los campos está vacío, muestra un mensaje de error o realiza alguna acción apropiada
-        alert("Por favor, llena los campos t1/2 ó T.");
+        K = 1 / -T;
+        mostrarResultadoK(K);
+        image1.style.display = "block";
+        image2.style.display = "none";
     }
+
+    var claseResultado = document.getElementById("divResultadosK");
+    claseResultado.style.display = "block";
 }
 
+
 function mostrarResultadoK(resultado) {
-    let resultadoElement = document.getElementById('resultadoK');
-    resultadoElement.textContent = resultado;
+    let resultadoElement = document.getElementById('K');
+    resultadoElement.value = resultado;
+    resultadoElement.disabled = true;
 }
 
 
@@ -48,6 +49,10 @@ function calculoT() {
     let auxK = document.getElementById('K').value;
     let T;
 
+    let op = prompt("¿Qué desea saber del tiempo?\n" +
+        "1. ¿Cuánta cantidad se ha sido desintegrado?\n" +
+        "2. ¿Cuánto queda?");
+
     if (auxK !== '' || K !== undefined) {
         T = 1 / K;
         alert(T.toFixed(3));
@@ -59,15 +64,10 @@ function calculoT() {
 
 function calculoNt() {
     let n0 = document.getElementById('n0').value;
-    let t = document.getElementById('t').value;
     K = document.getElementById('K').value;
 
     let Nt = n0 * (Math.pow(Math.E, (-K * t)));
     alert(Nt.toFixed(3));
-}
-
-function calculoN0() {
-
 }
 
 function calculoTiempo() {
@@ -84,43 +84,58 @@ function calculoTiempo() {
         divTiempo.style.display = 'block';
 
         let comboBox = document.getElementById("cuantoPoCSelect").value;
-        if(comboBox === "Porcentaje"){
+        if (comboBox === "Porcentaje") {
             let porcentaje = document.getElementById('cuantoPoC').value;
             let Nt = 1 - (porcentaje / 100);
             let K = document.getElementById('K').value;
             let n0 = document.getElementById('n0').value;
-    
-            let resultado = (Math.log(Nt / n0)) / -K;
+
+            let resultado = (Math.log(Nt / n0)) / K;
             alert(resultado.toFixed(3));
-        }else{
+        } else {
             let cantidad = document.getElementById('cuantoPoC').value;
             cantidad = 1 - cantidad;
             let K = document.getElementById('K').value;
 
-            let resultado = (Math.log(cantidad)) / -K;
+            let resultado = (Math.log(cantidad)) / K;
             alert(resultado.toFixed(3));
         }
     } else if (op == 2) {
         divTiempo.style.display = 'none';
         divQueda.style.display = 'block';
-        
+
         let comboBox = document.getElementById("cuantoQuedaSelect").value;
-        if(comboBox === "Porcentaje"){
+        if (comboBox === "Porcentaje") {
             let porcentaje = document.getElementById('cuantoQueda').value;
             porcentaje = porcentaje / 100;
             let K = document.getElementById('K').value;
             let n0 = document.getElementById('n0').value;
-    
-            let resultado = (Math.log(porcentaje / n0)) / -K;
+
+            let resultado = (Math.log(porcentaje / n0)) / K;
             console.log(resultado);
             alert(resultado.toFixed(3));
-        }else{
+        } else {
             let cantidad = document.getElementById("cuantoQueda").value;
             let K = document.getElementById('K').value;
 
-            let resultado = (Math.log(cantidad)) / -K;
+            let resultado = (Math.log(cantidad)) / K;
             console.log(resultado);
             alert(resultado.toFixed(3));
         }
     }
 }
+
+function inhabilitarTiempo() {
+    var selectElement = document.getElementById("tiempoGeneral");
+    selectElement.disabled = true;
+    selectElement = document.getElementById("tiempoGeneral");
+    var labelK = document.getElementById("tiempoK");
+    var labelThalf = document.getElementById("tiempoThalf");
+    var labelT = document.getElementById("tiempoT");
+    var selectedValue = selectElement.value;
+    labelK.innerHTML = selectedValue + "<sup>-1</sup>";
+    labelThalf.innerHTML = selectedValue;
+    labelT.innerHTML = selectedValue;
+}
+
+
